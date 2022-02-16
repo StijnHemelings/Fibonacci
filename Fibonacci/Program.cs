4e33be9a3a85a9f3c20793aec70480dbde6171cf
StijnHemelings <StijnHemelings@users.noreply.github.com>
2022-02-16 19:36:19 +0100

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Fibonacci");
+Console.WriteLine("********");
+Console.Write("Fill in a number:");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int first = 0;
+int second = 1;
+int third;
+
+Console.WriteLine();
+Console.WriteLine("The fibonacci of the number " + Number);
+Console.WriteLine(first);
+Console.WriteLine(second);
+
+for (int i = 0; i < Number - 2; i++)
+{
+    third = first + second;
+    Console.WriteLine(third);
+
+    first = second;
+    second = third;
+}
